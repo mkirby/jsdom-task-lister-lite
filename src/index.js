a9e42eebbd442200ef9013a6e058190555157cc8
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-03 17:25:05 -0500

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.querySelector("#create-task-form")
+  const taskListLi = document.querySelector("#tasks")
+
+  newTaskForm.addEventListener("submit", renderNewTask)
+  taskListLi.addEventListener("click", event => {
+    if (event.target.matches("li")) {
+      clearTask(event)
+    }
+  })
+
+  function clearTask(event) {
+    const item = event.target.textContent
+    if (confirm(`Are you sure you want to clear ${item}?`)) {
+      event.target.remove()
+    }
+  }
+
+  function renderNewTask(event) {
+    event.preventDefault()
+    const li = document.createElement("li")
+    li.textContent = event.target['new-task-description'].value
+    taskListLi.append(li)
+    newTaskForm.reset()
+  }
+
 });
